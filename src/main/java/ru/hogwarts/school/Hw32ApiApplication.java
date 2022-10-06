6ed928ff37f39eeb026013232f8fd083918c65ed
VMishutkin <vladmish@list.ru>
2022-10-06 12:42:17 +0700

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/Hw32ApiApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw32ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw32ApiApplication.class, args);
+    }
+
+}
