d98daaf8c0ac98af30b7c6f54e5f0972d8e80913
VMishutkin <vladmish@list.ru>
2022-10-21 11:42:26 +0700

    ВЫполнил дз 4.2? добавил файлы с запросами

--- scripts422.sql
+++ scripts422.sql
@@ -1,11 +1,11 @@
-CREATE TABLE user(
+CREATE TABLE users(
                      name VARCHAR(30),
                      age INTEGER CONSTRAINT age_constraint CHECK (age > 0),
                      is_licensed BOOLEAN DEFAULT FALSE,
                      car_id INTEGER REFERENCES car (car_id)
 );
 
-CREATE TABLE car(
+CREATE TABLE cars(
                     car_id INTEGER PRIMARY KEY,
                     brand VARCHAR(30),
                     model VARCHAR(30),
